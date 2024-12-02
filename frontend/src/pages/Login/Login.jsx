91f5d3c1cc55b1746b98e2514c3e7ccdf3c196de
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-12-02 14:28:29 -0600

    Add SignUp Page UI

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,31 +2,72 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../../components/input/PasswordInput'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+  
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [error, setError] = React.useState(null)
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email && !password) {
+      setError("All fields are required");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email')
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter a password')
+      return;
+    };
+    
+    setError("");
+
+    //Login Api Call
+  };
+
   return (
-    <div>
+    <>
+      <div>
         <Navbar />
 
         <div className="flex items-center justify-center mt-28">
             <div className="w-96 border rounded bg-white px-7 py-10">
-                <h4 className="text-2xl mb-7">Login</h4>
-                <input type="text" placeholder='Email' className="input-box" />
+                <form onSubmit={handleLogin}>
+                  <h4 className="text-2xl mb-7">Login</h4>
+                  <input type="text" placeholder='Email' className="input-box" 
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}  
+                  />
 
-                <PasswordInput/>
+                  <PasswordInput
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
 
-                <button type='submit' className="btn-primary">Login</button>
-                <p className="text-sm text-center mt-4">
-                    Not regidtered yet? {""}
+                  {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
-                    <Link to="/signUp" className='font-medium text-primary underline'>
-                    Create Acount
-                    </Link>
-                </p>
+                  <button type='submit' className="btn-primary">Login</button>
+                  <p className="text-sm text-center mt-4">
+                      Not registered yet? {""}
+
+                      <Link to="/signUp" className='font-medium text-primary underline'>
+                      Create Acount
+                      </Link>
+                  </p>
+                </form>
             </div>
         </div>
 
-    </div>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import PasswordInput from '../../components/input/PasswordInput'
+import { Link } from 'react-router-dom'
+import { validateEmail } from '../../utils/helper'
 
 const SignUp = () => {
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+
+  const handleSignUp = (e) => {
+    e.preventDefault()
+    if (!name && !email && !password) {
+      setError("All fields are required");
+      return;
+    }
+    if (!name){
+      setError("Name must be provided");
+    }
+    if (!validateEmail(email) ){
+      setError("Please enter a valid email");
+      return;
+    };
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      return;
+    }
+
+    setError("");
+    // Call API to sign up
+  }
+
+ 
+
   return (
-    <div>SignUp</div>
+    <>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+          <div className="w-96 border rounded bg-white px-7 py-10">
+              <form onSubmit={handleSignUp}>
+                <h4 className="text-2xl mb-7">SignUp</h4>
+
+                  <input 
+                  type="text" 
+                  placeholder='Name' 
+                  className="input-box" 
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}  
+                  />
+
+                  <input type="text" placeholder='Email' className="input-box" 
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}  
+                  />
+
+                  <PasswordInput
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />  
+
+                  {error && <p className="text-red-500 text-sm">{error}</p>}
+
+                  <button type='submit' className="btn-primary">Create Account</button>
+                  <p className="text-sm text-center mt-4">
+                      Are you already registered? {""}
+
+                      <Link to="/login" className='font-medium text-primary underline'>
+                      Log In
+                      </Link>
+                  </p>
+
+                </form>
+          </div>
+      </div> 
+    </> 
   )
 }
 
