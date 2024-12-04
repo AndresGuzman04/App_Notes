100ed4fe7e1b29672acc0ed362d40ff3199f254a
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-12-03 20:42:33 -0600

    Add NoteCard Component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 const Home = () => {
   return (
-    <Navbar />
-    
+    <>
+      <Navbar />
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Title Card"
+            date="2017-12-01"
+            content="Content Card"
+            tags="#tags"
+            isPinned={true}
+            onDeleteNote={() => {}}
+            onEditNote={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
