d2f5009313f8c98e5d864272803bb42888bb449c
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-12-04 22:12:25 -0600

    Add TagInput Component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
+import AddEditNotes from './AddEditNotes'
+import Modal from 'react-modal'
 
 const Home = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShow: false,
+    type:"add",
+    data:null,
+  });
+
   return (
     <>
       <Navbar />
@@ -20,6 +30,38 @@ const Home = () => {
           />
         </div>
       </div>
+
+      <button 
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" 
+        onClick={()=>{
+          setOpenAddEditModal({isShow:true,type:"add",data:null});
+        }}
+      >
+        <MdAdd className='text-[32px] text-white'/>
+      </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShow}
+        onRequestClose={() => {
+          setOpenAddEditModal({ isShow: false, type: "add", data: null });
+        }}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.2)'
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-sm mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes
+          type={openAddEditModal.type}
+          data={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShow: false, type: "add", data: null });
+          }}
+        />
+      </Modal>
+
     </>
   )
 }
