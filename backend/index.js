b0bf0a9f6bdd56bc7f4592b6759e67a70655361c
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-07 22:56:19 -0600

    Add Login

--- backend/index.js
+++ backend/index.js
@@ -1,84 +1,122 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const config = require("./config.json");
-const mongoose = require("mongoose");
+const config = require('./config.json')
+const mongoose = require('mongoose')
 
-mongoose.connect(config.connectionString);
+mongoose.connect(config.connectionString)
 
-const User = require("./models/user.model");
+const User = require('./models/user.model')
 
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const jwt = require("jsonwebtoken");
-const {authenticateToken} = require("./utilities");
+const jwt = require('jsonwebtoken')
+const {authenticateToken} = require('./utilities')
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(
     cors({
-        origin: ['+'],
-        credentials: true
+      origin: ['+'],
+      credentials: true
     })
-);
+)
 app.get('/', (req, res) => {
-    res.json({data: "Hello"});
+  res.json({data: 'Hello'})
 })
 
-// Create account 
-app.post("/create-account", async (req, res) => {
+// Create account
+app.post('/create-account', async (req, res) => {
+  const {name, email, password} = req.body
 
-    const {name, email, password} = req.body; 
-
-    if (!name) {
-        return res 
+  if (!name) {
+    return res
         .status(400)
-        .json({error: true , message: "Please enter your full name"});
-    }
+        .json({error: true, message: 'Please enter your full name'})
+  }
 
-    if (!email) {
-        return res 
+  if (!email) {
+    return res
        .status(400)
-       .json({error: true, message: "Please enter your email address"});
-    }
+       .json({error: true, message: 'Please enter your email address'})
+  }
 
-    if (!password) {
-        return res 
+  if (!password) {
+    return res
        .status(400)
-       .json({error: true, message: "Please enter your password"});
-    }
+       .json({error: true, message: 'Please enter your password'})
+  }
 
-    const isUser = await User.findOne({email: email});
+  const isUser = await User.findOne({email: email})
 
-    if (isUser) {
-        return res.json({
-            error: true,
-            message: "Email already exists."
-        });
-    }
-    
-    const user = new User({
-        name,
-        email,
-        password
-    });
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: 'Email already exists.'
+    })
+  }
 
-    await user.save();
+  const user = new User({
+    name,
+    email,
+    password
+  })
 
-    const accessToken = jwt.sign({user}, process.env.accessToken, {
-        expiresIn: "36000m"
-    });
+  await user.save()
 
-    res.json({
-        error: false,
-        user,
-        accessToken,
-        message: "User created successfully"
-    });
+  const accessToken = jwt.sign({user}, process.env.SECRET_KEY, {
+    expiresIn: '36000m'
+  })
 
-});
+  res.json({
+    error: false,
+    user,
+    accessToken,
+    message: 'User created successfully'
+  })
+})
 
-app.listen(8000)
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
 
-module.exports = app;
+  if (!email) {
+    return res.status(400).json({message: 'Email is required'})
+  }
+
+  if (!password) {
+    return res.status(400).json({message: 'Password is required'})
+  }
+
+  const userInfo = await User.findOne({email: email})
+  if (!userInfo) {
+    return res.status(400).json({message: 'User not found'})
+  }
+
+  if (userInfo.email === email && userInfo.password === password) {
+    const user = {user: userInfo}
+    const accessToken = jwt.sign(user, process.env.SECRET_KEY, {
+      expiresIn: '36000m'
+    })
+    return res.json({
+      error: false,
+      email,
+      accessToken,
+      message: 'User logged in successfully'
+    })
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: 'Invalid email or password'
+    })
+  }
+})
+
+app.post('/add-notes', authenticateToken, async (req, res) => {})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+module.exports = app
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
- const userSchema = new Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    createOn: {type: Date, default: new Date().getTime()}
- });
+const userSchema = new Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  createOn: {type: Date, default: new Date().getTime()}
+})
 
- module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
